d15e741e6baa622efedde6df5d76bdf4424b5d20
Luck † <lukki12@gmail.com>
2023-02-07 11:21:00 -0300

    add reset and global styles/ estructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import { ToastContainer } from "react-toastify";
 
+
+function App() {
   return (
     <div className="App">
-    
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/styles/reset.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Reset = createGlobalStyle`
+
+*{
+    box-sizing: 0;
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    list-style: none;
+    text-decoration: none;
+}
+
+
+button{
+    cursor: pointer;
+    background: transparent;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+a,
+span,
+li,
+button,
+label,
+input,body {
+  font-family: "Inter", sans-serif;
+}
+
+`;
